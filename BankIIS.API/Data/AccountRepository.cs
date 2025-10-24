40e5d291bfffe1730924880cc82a980fb8e6d321
Eric J. Reid <openroadco@gmail.com>
2025-10-24 14:31:14 -0500

    Account repository (type-specific)

---
+++ BankIIS.API/Data/AccountRepository.cs
@@ -0,0 +1,59 @@
+﻿using BankIIS.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BankIIS.API.Data
+{
+	public class AccountRepository(BankDbContext context)
+	{
+		private BankDbContext db = context;
+
+		public List<Account> Get()
+		{
+			return db.Accounts.ToList();
+		}
+
+		public Account Find(int? id)
+		{
+			return db.Accounts.Find(id);
+		}
+
+		public void Add(Account account) 
+		{ 
+			db.Accounts.Add(account);
+			db.SaveChanges();
+		}
+
+		public void Update(Account account)
+		{
+			db.Entry(account).State = EntityState.Modified;
+			db.SaveChanges();
+		}
+
+		public void Remove(Account account)
+		{
+			db.Accounts.Remove(account);
+			db.SaveChanges();
+		}
+
+		private bool disposed = false;
+
+		protected virtual void Dispose(bool disposing) 
+		{
+			if (!this.disposed)
+			{
+				if (disposing)
+				{
+					db.Dispose();
+				}
+			}
+			this.disposed = true;
+		}
+
+		public void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+
+	}
+}
---
+++ BankIIS.API/Data/GenericRepository.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BankIIS.API.Data
+{
+	public class GenericRepository<TEntity>(BankDbContext context) where TEntity : class
+	{
+		private BankDbContext db = context;
+
+		public virtual List<TEntity> Get()
+		{
+			return db.Set<TEntity>().ToList();
+		}
+
+		public TEntity Find(object id) 
+		{
+			return db.Set<TEntity>().Find(id);
+		}
+
+		public void Add(TEntity entity)
+		{
+			db.Set<TEntity>().Add(entity);
+			db.SaveChanges();
+		}
+
+		public void Update(TEntity entity)
+		{
+			db.Entry(entity).State = EntityState.Modified;
+			db.SaveChanges();
+		}
+
+		public void Remove(TEntity entity)
+		{
+			db.Set<TEntity>().Remove(entity);
+			db.SaveChanges();
+		}
+
+		public void Remove(object id)
+		{
+			var entity = db.Set<TEntity>().Find(id);
+			Remove(entity);
+		}
+
+		private bool disposed = false;
+
+		protected virtual void Dispose(bool disposing)
+		{
+			if (!this.disposed)
+			{
+				if (disposing)
+				{
+					db.Dispose();
+				}
+			}
+			this.disposed = true;
+		}
+
+		public void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+
+	}
+}
